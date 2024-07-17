8113baadba500e5f52cb7f4ece147129d193d63c
tessaSlice <awamy10101010@gmail.com>
2024-07-17 18:19:31 -0400

    configured astro to work for github pages

--- density-debris/astro.config.mjs
+++ density-debris/astro.config.mjs
@@ -7,5 +7,7 @@ export default defineConfig({
   integrations: [mdx(), icon()],
   content: {
     collections: './src/content/config.ts' // Point to your content config file
-  }
+  },
+  site: 'https://tessaSlice.github.io',
+  base: '/density-debris/'
 });

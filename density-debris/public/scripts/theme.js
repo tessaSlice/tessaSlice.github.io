dae401b17a3f6c76cf2f334ff2c43b17d7e173bb
tessaSlice <awamy10101010@gmail.com>
2024-07-19 20:03:06 -0400

    added dark mode preference setting

--- density-debris/public/scripts/theme.js
+++ density-debris/public/scripts/theme.js
@@ -1,7 +1,19 @@
 (function () {
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    document.documentElement.setAttribute('data-theme', savedTheme);
-    if (savedTheme === 'dark') {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const savedTheme = localStorage.getItem('theme');
+
+    let theme = 'light';
+    // first check if they prefer dark mode
+    if (prefersDark) {
+        theme = 'dark';
+    }
+    // then check if they have a saved theme (this has higher priority)
+    if (savedTheme) {
+        theme = savedTheme;
+    }
+
+    document.documentElement.setAttribute('data-theme', theme);
+    if (theme === 'dark') {
         document.documentElement.style.setProperty('--background-color', 'var(--dark-background-color)');
         document.documentElement.style.setProperty('--text-color', 'var(--dark-text-color)');
         document.documentElement.style.setProperty('--button-background-color', 'var(--dark-button-background-color)');
